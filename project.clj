191d079b266bb3af8cd3894cd5c43bea3ed5328d
Daehyun Kim <hatemogi@gmail.com>
2016-09-15 14:48:06 +0900

    부트스트랩 골격 추가

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [misaeng "0.1.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler misaeng-lang.org.handler/app}
   :profiles
--- src/misaeng_lang/org/handler.clj
+++ src/misaeng_lang/org/handler.clj
@@ -1,10 +1,37 @@
 (ns misaeng-lang.org.handler
-  (:require [compojure.core :refer :all]
+  (:require [미생.기본 :refer :all]
+            [compojure.core :refer :all]
             [compojure.route :as route]
+            [hiccup.page :refer [html5 include-css include-js]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn layout
+  "HTML 기본 레이아웃"
+  [& contents]
+  (html5 [:head
+          [:meta {:charset "utf-8"}]
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+          [:title "미생 - 실험적 한글 프로그래밍 언어"]
+          (map include-css
+               ["https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+                "http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css"
+                "css/misaeng.css"])]
+         (-> [:body]
+             (into contents)
+             (into (map include-js ["https://code.jquery.com/jquery-3.1.0.min.js"
+                                    "https://d3js.org/d3.v4.min.js"
+                                    "http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"
+                                    "js/misang.js"])))))
+
+(defn page
+  [& contents]
+  (layout [:nav [:div "페이지 헤더"]]
+          (into [:main] contents)
+          [:footer "Copyrights 2016 Daehyun Kim"]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (page "Hello World"))
   (route/not-found "Not Found"))
 
 (def app
